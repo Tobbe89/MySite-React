dd9f1a03179f52ca0d334906564d6b42762026ed
Tobbe <tobias.persson@sigma.se>
2022-07-16 19:19:06 +0200

    animation fixed

--- src/Components/Experience/Education.js
+++ src/Components/Experience/Education.js
@@ -4,27 +4,30 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 
 function Education() {
-  useEffect(() => {
-    Aos.init({ duration: 2000 });
-  }, []);
+
 
   return (
     <section id="education">
-      <div data-aos="fade-left" className="text-container">
-        <h2>Fullstack Developer .NET</h2>
-        <p>Software- , Web- , System- and Application-Developer</p>
-        {/* <p>Visual Studio: ASP.NET, MVC, WPF, Unit Test</p>
+      <div className="text-container">
+        <div data-aos="fade-left" data-aos-duration="1500" >
+          <h2>Fullstack Developer .NET</h2>
+          <p>Software- , Web- , System- and Application-Developer</p>
+          {/* <p>Visual Studio: ASP.NET, MVC, WPF, Unit Test</p>
         <p>Visual Studio Code: JS, HTML, CSS, JSx, React</p> */}
-        {/* <p>SQL, Postman</p> */}
-        <h2>Microcomputer technology, Göteborgs Tekniska Institut.</h2>
-        <p>C, C++ och Assembly Programming</p>
-
-        <h2>It-Media , John Bauer - High School.</h2>
-        <p>
-          Managing media content such as: News, Filming, Photoshoping, Virtual
-          environments etc. Programs: Word, Excel, PowerPoint, Photoshop,
-          Illustrator.
-        </p>
+          {/* <p>SQL, Postman</p> */}
+        </div>
+        <div data-aos="fade-left" data-aos-duration="1600" >
+          <h2>Microcomputer technology, GTI</h2>
+          <p>C, C++ och Assembly Programming</p>
+        </div>
+        <div data-aos="fade-left" data-aos-duration="1700" >
+          <h2>It-Media , John Bauer - High School</h2>
+          <p>
+            Managing media content such as: News, Filming, Photoshoping, Virtual
+            environments etc. Programs: Word, Excel, PowerPoint, Photoshop,
+            Illustrator
+          </p>
+        </div>
       </div>
       <div className="picture-container"></div>
     </section>
--- src/Components/Experience/TechExperience.js
+++ src/Components/Experience/TechExperience.js
@@ -4,27 +4,33 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 
 function TechExperience() {
-  useEffect(() => {
-    Aos.init({ duration: 2000 });
-  }, []);
+  // useEffect(() => {
+  //   Aos.init({ duration: 2000 });
+  // }, []);
 
   return (
     <section id="techexp">
-      <div data-aos="fade-left" className="text-container">
-        <h2 className="contentText">Nexer</h2>
-        <p>
-          I had my intership and now my fulltime job as developer at Nexer, Örebro Sweden.
-        </p>
-        <h2 className="contentText">Blue Social</h2>
-        <p>
-          For a short time I was programming NFC-Cards for Blue Social in
-          Riverside, CA.
-        </p>
-        <h2 className="contentText">Ericsson</h2>
-        <p>
-          Final assembler/Testoperator. For some time I was Assembling, Testing
-          and Troubleshooting radio units for Ericsson.
-        </p>
+      <div className="text-container">
+        <div data-aos="fade-left" data-aos-duration="1500" >
+          <h2 className="contentText">Nexer</h2>
+          <p>
+            I had my intership and now my fulltime job as developer at Nexer, Örebro Sweden
+          </p>
+        </div>
+        <div data-aos="fade-left" data-aos-duration="1600" >
+          <h2 className="contentText">Blue Social</h2>
+          <p>
+            For a short time I was programming NFC-Cards for Blue Social in
+            Riverside, CA
+          </p>
+        </div>
+        <div data-aos="fade-left" data-aos-duration="1700" >
+          <h2 className="contentText">Ericsson</h2>
+          <p>
+            Final assembler/Testoperator. For some time I was Assembling, Testing
+            and Troubleshooting radio units for Ericsson
+          </p>
+        </div>
       </div>
       <div className="picture-container">
         {/* <div data-aos="fade-left" className="nexer-logo">
